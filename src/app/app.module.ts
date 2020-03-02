59296c8b7638f2e45b8cdbdd8b6707bf0f96c3f4
NenetteGit <antonin.texier@gmail.com>
2020-03-02 18:45:55 +0100

    feat(modules): Integrate notification module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NotifierModule } from 'angular-notifier';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MonacoEditorModule } from 'ngx-monaco-editor';
@@ -35,6 +36,46 @@ import { NotFoundComponent } from './not-found/not-found.component';
     ReactiveFormsModule,
     FontAwesomeModule,
     MonacoEditorModule.forRoot(),
+    NotifierModule.withConfig({
+      position: {
+        horizontal: {
+          position: "right",
+          distance: 12
+        },
+        vertical: {
+          position: "top",
+          distance: 12,
+          gap: 10
+        }
+      },
+      theme: "material",
+      behaviour: {
+        autoHide: 5000,
+        onClick: false,
+        onMouseover: "pauseAutoHide",
+        showDismissButton: true,
+        stacking: 4
+      },
+      animations: {
+        enabled: true,
+        show: {
+          preset: "slide",
+          speed: 300,
+          easing: "ease"
+        },
+        hide: {
+          preset: "fade",
+          speed: 300,
+          easing: "ease",
+          offset: 50
+        },
+        shift: {
+          speed: 300,
+          easing: "ease"
+        },
+        overlap: 150
+      }
+    }),
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
