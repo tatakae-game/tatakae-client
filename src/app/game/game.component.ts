7c1582d3f2fff8061c7fdcee09ed9c8f06608cbf
quantumsheep <nathanael.dmc@outlook.fr>
2020-03-17 16:01:55 +0100

    feat(game): Add animated sprites and texture alternatives

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,30 @@
 import { Component, OnInit, ElementRef, NgZone } from '@angular/core';
 import * as PIXI from 'pixi.js';
 
+interface TileSettings {
+  texture?: string[];
+  textures?: string[][];
+}
+
+const TILES: { [key: string]: TileSettings } = {
+  'grass': {
+    texture: [
+      '/assets/tiles/grass.png',
+    ],
+  },
+  'tree': {
+    textures: [
+      [
+        '/assets/tiles/tree/tree1.png',
+        '/assets/tiles/tree/tree2.png',
+        '/assets/tiles/tree/tree3.png',
+        '/assets/tiles/tree/tree4.png',
+        '/assets/tiles/tree/tree5.png',
+      ],
+    ],
+  },
+}
+
 interface Tilemap {
   tiles: string[];
 }
@@ -65,15 +89,18 @@ export class GameComponent implements OnInit {
         .map(tilemap => new Set(tilemap.tiles))
         .reduce((a, v) => a.concat(...v), [])
 
-      const unique_tiles = new Set(unique_tilemaps_tiles)
+      const unique_tiles = new Set<string>(unique_tilemaps_tiles);
+      [...unique_tiles].filter(tile => !!tile).forEach(tile => {
+        const tile_settings = TILES[tile];
 
-      loader.add([...unique_tiles].map(tile => `/assets/tiles/${tile}.png`));
-
-      loader.on('complete', () => {
-        this.loadGame();
+        if (tile_settings) {
+          loader.add(tile_settings.textures || tile_settings.texture)
+        }
       });
 
-      loader.load();
+      loader.load(() => {
+        this.loadGame();
+      });
     });
 
     this.elementRef.nativeElement.appendChild(this.app.view);
@@ -95,20 +122,17 @@ export class GameComponent implements OnInit {
 
         if (!tile) continue;
 
-        const texture = PIXI.Texture.from(`/assets/tiles/${tile}.png`);
-        texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST
+        const tile_settings = TILES[tile];
+        const sprite = this.generateSprite(tile_settings);
 
-        const sprite = new PIXI.Sprite(texture);
-        const sprite_x_scale = scale / texture.orig.width;
-        const sprite_y_scale = scale / texture.orig.height;
+        const sprite_x_scale = scale / sprite.texture.orig.width;
+        const sprite_y_scale = scale / sprite.texture.orig.height;
 
         sprite.scale.set(sprite_x_scale, sprite_y_scale);
         sprite.anchor.set(0.5, 0.5);
 
         sprite.x = (x + 0.5) * scale;
         sprite.y = (y + 0.5) * scale;
-
-        console.log(sprite.x, sprite.y)
 
         if (index === 0) {
           sprite.angle = Math.floor(Math.random() * 3) * 90;
@@ -125,5 +149,32 @@ export class GameComponent implements OnInit {
     // Center bunny sprite in local container coordinates
     container.pivot.x = 0;
     container.pivot.y = 0;
+  }
+
+  /**
+   * @param {TileSettings} tile_settings 
+   */
+  generateSprite(tile_settings) {
+    if (tile_settings.textures) {
+      const alternatives = tile_settings.textures[Math.floor(Math.random() * tile_settings.textures.length)];
+      const textures = alternatives.map(path => {
+        const texture = PIXI.Texture.from(path);
+        texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST
+
+        return texture
+      });
+
+      const sprite = new PIXI.AnimatedSprite(textures)
+      sprite.animationSpeed = 0.075;
+      sprite.play();
+
+      return sprite;
+    } else {
+      const alternative = tile_settings.texture[Math.floor(Math.random() * tile_settings.texture.length)];
+      const texture = PIXI.Texture.from(alternative);
+      texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST
+
+      return new PIXI.Sprite(texture)
+    }
   }
 }
