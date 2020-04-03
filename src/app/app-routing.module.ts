3ffea2eb5a882ca38708d28bcd9b85f9fe16c50e
NenetteGit <antonin.texier@gmail.com>
2020-04-03 18:52:39 +0200

    fix(routing):  Delete missing component access.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,6 @@ import { LoginComponent } from './views/login/login.component';
 import { NotFoundComponent } from './views/not-found/not-found.component';
 import { RoomComponent } from './views/room/room.component';
 import { RoomsComponent } from './views/rooms/rooms.component';
-import { ChatComponent } from './views/chat/chat.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [NoAuthGuard], },
