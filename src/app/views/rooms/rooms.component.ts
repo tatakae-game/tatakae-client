6a8f9702bd0cd1c716aad5bc3b729c3889615e88
NenetteGit <antonin.texier@gmail.com>
2020-04-06 17:29:53 +0200

    feat(chat): Add searching feature to create chat room.

--- src/app/views/rooms/rooms.component.ts
+++ src/app/views/rooms/rooms.component.ts
@@ -4,6 +4,10 @@ import { Room } from 'src/app/models/room.model';
 import { RoomsService } from 'src/app/rooms.service';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
+import { UsersService } from 'src/app/services/users.service';
+import { Session } from 'src/app/models/session.model';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-rooms',
@@ -16,14 +20,20 @@ export class RoomsComponent implements OnInit {
   newTicketForm: FormGroup;
   isSupportPage: boolean;
   redirectToUrl: string;
+  searchedUsers: User[] = [];
+  session: Session;
 
   constructor(private roomService: RoomsService,
     private router: Router,
     private formBuilder: FormBuilder,
+    private usersService: UsersService,
+    private authService: AuthService,
   ) { }
 
   ngOnInit(): void {
     this.isSupportPage = this.router.url.includes('support');
+
+    this.session = this.authService.session();
 
     this.newRoomForm = this.formBuilder.group({
       name: ['', Validators.required],
@@ -62,5 +72,12 @@ export class RoomsComponent implements OnInit {
         this.getRooms();
       }
     });
+  }
+
+  async onInviteUserUpdate(value: string) {
+    if (value.length > 0) {
+      const users = await this.usersService.searchUsers(value);
+      this.searchedUsers = users.filter(user => user.username !== this.session.user.username);
+    }
   }
 }
