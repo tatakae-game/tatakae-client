ec74d02cab6918e46ca95537446a28d02b3fb201
NenetteGit <antonin.texier@gmail.com>
2020-04-27 11:34:33 +0200

    feat(admin): Create new component permissions' groups

---
+++ src/app/views/dashboard/permission-groups/permission-groups.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-permission-groups',
+  templateUrl: './permission-groups.component.html',
+  styleUrls: ['./permission-groups.component.scss']
+})
+export class PermissionGroupsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
