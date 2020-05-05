7477e79b8b936cff99632a7ff37df945898ff0cb
NenetteGit <antonin.texier@gmail.com>
2020-05-05 23:44:43 +0200

    feat(groups): Implement view and logic of permissions groups.

--- src/app/views/dashboard/permission-groups/permission-groups.component.ts
+++ src/app/views/dashboard/permission-groups/permission-groups.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { PermissionGroupsService } from 'src/app/services/permission-groups.service';
+import { Group } from 'src/app/models/group.model';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { faPlusSquare, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { NotifierService } from 'angular-notifier';
+import { MatDialog } from '@angular/material/dialog';
+import { PermissionsGroupDialogComponent } from '../../dialogs/permissions-groups-dialog/permissions-groups-dialog.component';
 
 @Component({
   selector: 'app-permission-groups',
@@ -7,9 +16,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PermissionGroupsComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['name', 'permissions'];
+  groups: MatTableDataSource<Group>;
 
-  ngOnInit(): void {
+  addIcon = faPlusSquare;
+  editIcon = faEdit;
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private groupsService: PermissionGroupsService,
+    private notifierService: NotifierService,
+    public dialog: MatDialog,
+  ) { }
+
+  async ngOnInit() {
+    await this.getGroups();
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.groups.filter = filterValue.trim().toLowerCase();
+  }
+
+  async getGroups() {
+    try {
+      const response = await this.groupsService.getAllGroups();
+      this.groups = new MatTableDataSource<Group>(response.groups);
+      this.groups.sort = this.sort;
+      this.groups.paginator = this.paginator;
+    } catch (error) {
+      this.notifierService.notify('error', error.message);
+    }
+  }
+
+  async updateGroup(group: Group) {
+    try {
+      await this.groupsService.updateGroup(group);
+    } catch (error) {
+      this.notifierService.notify('error', error.message);
+    }
+  }
+
+  openAddDialog(): void {
+    const dialogRef = this.dialog.open(PermissionsGroupDialogComponent, {
+      width: '250px',
+      data: { title: 'Add', name: '' }
+    });
+
+    dialogRef.afterClosed().subscribe(async (data: string) => {
+      if (data !== undefined && data !== '') {
+        try {
+          await this.groupsService.addGroup(data);
+          await this.getGroups();
+          this.groups.paginator.lastPage();
+
+        } catch (error) {
+          this.notifierService.notify('error', error.message);
+        }
+      }
+    });
+  }
+
+  openEditDialog(group: Group): void {
+    const dialogRef = this.dialog.open(PermissionsGroupDialogComponent, {
+      width: '250px',
+      data: { title: 'Update', name: group.name }
+    });
+
+    dialogRef.afterClosed().subscribe(async (data: string) => {
+      if (data !== undefined && data !== '') {
+        try {
+          if (group.name !== data) {
+            group.name = data;
+            await this.groupsService.updateGroup(group);
+            await this.getGroups();
+          }
+
+        } catch (error) {
+          this.notifierService.notify('error', error.message);
+        }
+      }
+    });
   }
 
 }
