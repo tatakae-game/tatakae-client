78e52d0422a88d4b7d8809047a0074392d66441b
Simon Halimi <simonhalimi@Simons-MBP.home>
2020-06-27 13:42:26 +0200

    feat(): Add user code recuperation

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import config from '../config';
+import { Session } from '../models/session.model';
 
 import { ApiResponse } from '../api-response';
 
@@ -27,5 +28,34 @@ export class UsersService {
     } catch {
       return null;
     }
+  }
+
+  async getMe() {
+    try {
+      const session = this.session()
+      return session.user
+    } catch {
+      return [];
+    }
+  }
+
+  async getUserCode(type: String) {
+    try {
+      const session = this.session()
+      const res = await this.http.get<ApiResponse>(`${config.api_url}/users/code/${type}`).toPromise();
+      return res.code || [];
+    } catch {
+      return [];
+    }
+  }
+
+  session(): Session {
+    const session = localStorage.getItem('session')
+
+    if (!session) {
+      return null;
+    }
+
+    return JSON.parse(atob(session))
   }
 }
