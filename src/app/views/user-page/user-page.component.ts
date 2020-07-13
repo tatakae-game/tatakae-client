e92f6c46a7f703a7bad0ca3d269fe3d8fd4e2ad7
Simon Halimi <halimisimon@gmail.com>
2020-07-13 16:33:09 +0200

    feat(): can modify password - need to handle error messaged

--- src/app/views/user-page/user-page.component.ts
+++ src/app/views/user-page/user-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/services/users.service';
 import { User } from 'src/app/models/user.model';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-page',
@@ -10,15 +11,28 @@ import { User } from 'src/app/models/user.model';
 export class UserPageComponent implements OnInit {
 
   user: User;
+  changePasswordForm: FormGroup;
+  newPassword: FormControl;
+  password: FormControl;
 
-  constructor(private userService: UsersService ) { }
+  constructor(private userService: UsersService, private formBuilder: FormBuilder) {
+    this.changePasswordForm = this.formBuilder.group({
+      newPassword: ['', Validators.required],
+      password: ['', Validators.required],
+    }
+    );
+  }
 
   async ngOnInit(): Promise<void> {
     this.user = await this.userService.getMe()
   }
 
-  update_default_language(selected_language: string){
+  update_default_language(selected_language: string) {
     this.userService.update_user_language(selected_language)
+  }
+
+  async onSubmit(value: any) {
+    const res = await this.userService.update_password(value.password, value.newPassword);
   }
 
 }
