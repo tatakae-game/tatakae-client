f1df6781abfc55734c70e53a1cfbbd5667754df0
Simon Halimi <halimisimon@gmail.com>
2020-07-20 01:16:07 +0200

    fix: change test for true

--- src/app/views/editor/editor.component.ts
+++ src/app/views/editor/editor.component.ts
@@ -79,7 +79,7 @@ export class EditorComponent implements OnInit {
 
   testCode() {
     this.displayed.code = this.code;
-    this.game.run(this.files, this.language, "test");
+    this.game.run(this.files, this.language, "true");
   }
 
   async saveCode() {
