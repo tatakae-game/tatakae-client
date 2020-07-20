a0389f26cd1473f40f2eae9fb26a55eb0f252ed6
Simon Halimi <halimisimon@gmail.com>
2020-07-20 03:04:26 +0200

    feat(): Add error mesage on last file deleted

--- src/app/views/editor/editor.component.ts
+++ src/app/views/editor/editor.component.ts
@@ -129,7 +129,7 @@ export class EditorComponent implements OnInit {
 
   deleteFile() {
     if (this.files.length <= 1) {
-      return console.log(`can't delete last file`)
+      return this.notifierService.notify('error', 'You cannot delete the last file');
     }
 
     this.files = this.files.filter(file => file !== this.displayed)
