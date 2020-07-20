be01a1a5282ba9ff4445637929d9b2e339eb5f2a
Simon Halimi <halimisimon@gmail.com>
2020-07-21 00:53:00 +0200

    feat(): Add environment context api call

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,7 @@
 import { ENV } from 'pixi.js';
 import { env } from 'process';
+import { environment } from 'src/environments/environment';
 
 export default {
-    api_url: env.API_URL || 'http://localhost:3000'
+    api_url: environment.api_url
 }
